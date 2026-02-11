ff1b65534ffa477cbc2c6b9d8e70bea2f067c241
RanishkaGunathilake <ranishkagunathilake@gmail.com>
2026-02-11 11:06:00 +0530

    Initial commit

--- app/src/main/java/com/example/medicinereminderapplication/HomeActivity.java
+++ app/src/main/java/com/example/medicinereminderapplication/HomeActivity.java
@@ -8,17 +8,63 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.content.Intent;
+import android.widget.Button;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+import java.util.ArrayList;
+
 public class HomeActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    MedicineAdapter adapter;
+    DatabaseHelper dbHelper;
+    Button btnLogout;
+    String username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        username = getIntent().getStringExtra("username");
+
+        recyclerView = findViewById(R.id.recyclerMedicines);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        dbHelper = new DatabaseHelper(this);
+
+        List<Medicine> medicines = dbHelper.getMedicineForUser(username);
+        if (medicines == null) medicines = new ArrayList<>();
+
+        adapter = new MedicineAdapter(medicines, this, medicine -> {
+            Intent intent = new Intent(HomeActivity.this, AddEditMedicineActivity.class);
+            intent.putExtra("medicineId", medicine.getId());
+            intent.putExtra("username", username);
+            startActivity(intent);
+        });
+
+        recyclerView.setAdapter(adapter);
+
+        findViewById(R.id.fabAddMedicine).setOnClickListener(v -> {
+            Intent intent = new Intent(HomeActivity.this, AddEditMedicineActivity.class);
+            Intent.putExtra("username", username);
+            startActivity(intent);
+        });
+
+        btnLogout = findViewById(R.id.btnLogout);
+        btnLogout.setOnClickListener(v -> {
+            startActivity(new Intent(HomeActivity.this, LoginActivity.class));
+            finish();
         });
     }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        adapter.updateList(dbHelper.getMedicinesForUser(username));
+    }
 }
+
+//Completed
